b5f608b5e0c7f8dee0662a14cf0fc305cce30459
Benjamin Holmes <bholmes@redhat.com>
2014-08-12 17:27:35 +0100

    added control scripts and updated welcome page

---
+++ template/src/main/java/welcome/Welcome.java
@@ -0,0 +1,25 @@
+package welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class Welcome {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Welcome to Spring Boot on OpenShift";
+    }
+    
+    @RequestMapping("/rest")
+  
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Welcome.class, args);
+    }
+}
